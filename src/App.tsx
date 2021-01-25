8138abecbed34271e59737ff034bb19154a32475
Patricia Paulsson <ppaulsson@rapidimages.se>
2021-01-25 14:30:39 +0100

    Removed unused variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled, { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components'
 
 import Header from './components/Header'
 import People from './components/People'
